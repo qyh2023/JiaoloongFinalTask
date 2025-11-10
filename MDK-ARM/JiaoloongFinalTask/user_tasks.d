a35bffe70be3df2f285c3f2a45d093e655684941
qyh2023 <qyh2023@sjtu.edu.cn>
2025-11-10 21:37:54 +0800

    feat: Semaphore

--- MDK-ARM/JiaoloongFinalTask/user_tasks.d
+++ MDK-ARM/JiaoloongFinalTask/user_tasks.d
@@ -12,4 +12,7 @@ jiaoloongfinaltask/user_tasks.o: ..\UserCode\Src\user_tasks.cpp \
   ..\Middlewares\Third_Party\FreeRTOS\Source\portable\GCC\ARM_CM4F\portmacro.h \
   ..\Middlewares\Third_Party\FreeRTOS\Source\include\mpu_wrappers.h \
   ..\UserCode\Inc\user_tasks.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\task.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\include\list.h \
   ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os2.h
--- UserCode/Src/user_tasks.cpp
+++ UserCode/Src/user_tasks.cpp
@@ -3,6 +3,8 @@
 //
 #include "FreeRTOS.h"
 #include "user_tasks.h"
+
+#include "cmsis_os.h"
 #include "cmsis_os2.h"  // cubemx里面选的2
 
 uint32_t count = 0;
@@ -17,11 +19,6 @@ osThreadAttr_t test_task_attributes = {
     .priority = osPriorityNormal // 任务优先级：普通优先级
 };
 
-osMessageQueueId_t test_queue_handle;
-osMessageQueueAttr_t test_queue_attributes = {
-    .name = "test_queue"
-};
-
 osThreadId_t test1_task_handle;
 osThreadAttr_t test1_task_attributes = {
     .name = "test1Task",          // 任务名称
@@ -29,27 +26,72 @@ osThreadAttr_t test1_task_attributes = {
     .priority = osPriorityNormal // 任务优先级：普通优先级
 };
 
+osThreadId_t test2_task_handle;
+osThreadAttr_t test2_task_attributes = {
+    .name = "testTask",          // 任务名称
+    .stack_size = 256 * 4,       // 堆栈大小：256*4=1024字节
+    .priority = osPriorityNormal // 任务优先级：普通优先级
+};
 
+osMessageQueueId_t test_queue_handle;
+osMessageQueueAttr_t test_queue_attributes = {
+    .name = "test_queue"
+};
+
+osSemaphoreId_t test_semaphore_handle;
+osSemaphoreAttr_t test_semaphore_attributes = {
+    .name = "test_semaphore"
+};
+
+// 给对列push东西的线程
+// void test_task(void *) {
+//     while (true) {
+//         const auto tick = osKernelGetTickCount();
+//         // ++count;
+//         send += 1;
+//         osMessageQueuePut(test_queue_handle, &send, 0, 0);  // timeout:如果队列满了的阻塞时间。但这里表示满了不阻塞，直接返回失败
+//         osDelayUntil(tick + 1);
+//     }
+// }
+
+// 释放信号量的线程
 void test_task(void *) {
     while (true) {
         const auto tick = osKernelGetTickCount();
         // ++count;
-        send += 1;
-        osMessageQueuePut(test_queue_handle, &send, 0, 0);  // timeout:如果满了的阻塞时间。但这里表示满了不阻塞，直接返回失败
+        if (send++ % 5 == 0) {
+            osSemaphoreRelease(test_semaphore_handle);  // 释放信号量
+        }
         osDelayUntil(tick + 1);
     }
 }
 
-void test1_task(void *) {
+// void test1_task(void *) {
+//     while (true) {
+//         osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);  //如果队列为空，任务会一直阻塞
+//     }
+// }
+
+// 获取信号量并做事的线程
+void test2_task(void *) {
     while (true) {
-        osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);  //如果队列为空，任务会一直阻塞
+        osSemaphoreAcquire(test_semaphore_handle, osWaitForever);
+        recv++;
     }
 }
 
-
 void user_tasks_init() {
-    test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);
-    test1_task_handle = osThreadNew(test1_task, nullptr, &test1_task_attributes);
-    test_queue_handle = osMessageQueueNew(16, sizeof(uint32_t), &test_queue_attributes);  // 新建了message
+    // 1.消息队列：
+    // test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);  //线程test_task_handle，向 test_queue_handle 队列中push
+    // test1_task_handle = osThreadNew(test1_task, nullptr, &test1_task_attributes);  // 线程test1_task_handle，从 test1_task_attributes 中pull
+
+    // 消息队列ID = new(消息数量，消息大小，属性)
+    // test_queue_handle = osMessageQueueNew(16, sizeof(uint32_t), &test_queue_attributes);  // 新建了message
+
+    // 2.信号量
+    test_task_handle = osThreadNew(test_task, nullptr, &test_task_attributes);  //线程test_task_handle，释放信号量
+    test2_task_handle = osThreadNew(test2_task, nullptr, &test1_task_attributes);  // 线程test2_task_handle，获取信号量
+
+    test_semaphore_handle = osSemaphoreNew(1, 0, &test_semaphore_attributes);
 
 }
